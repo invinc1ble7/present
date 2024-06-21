9243820f6860297d781f18747fd64f1437a38244
invinc1ble7 <93664486+invinc1ble7@users.noreply.github.com>
2024-06-21 18:29:07 +0200

    Aktualisieren von present.sh

--- present.sh
+++ present.sh
@@ -1,3 +1,86 @@
 yes | pkg up && yes | pkg install python
-bash <(curl -s https://raw.githubusercontent.com/invinc1ble7/present/main/present.py)
+echo "import time
+
+# ANSI escape sequence for vibrant purple/violet color
+purple_color = "\033[35m"
+
+text = "Hallo Medeea, ich hoffe es geht dir gut."
+output = ""
+
+for char in text:
+    output += char
+    print(purple_color + output, end="\r", flush=True)
+    time.sleep(0.05)
+
+print("\n")
+
+text = "Heute ist unser 10. Monatstag"
+output = ""
+
+for char in text:
+    output += char
+    print(purple_color + output, end="\r", flush=True)
+    time.sleep(0.05)
+
+print("\n")
+
+text = "Die Zeit vergeht doch so schnell, oder?"
+output = ""
+
+for char in text:
+    output += char
+    print(purple_color + output, end="\r", flush=True)
+    time.sleep(0.05)
+
+print("\n")
+
+text = "Ich liebe dich so sehr Baby <333"
+output = ""
+
+for char in text:
+    output += char
+    print(purple_color + output, end="\r", flush=True)
+    time.sleep(0.05)
+
+print("\n")
+
+text = "Hier ein kleines Geschenk für dich :3"
+output = ""
+
+for char in text:
+    output += char
+    print(purple_color + output, end="\r", flush=True)
+    time.sleep(0.075)
+
+print("\n")
+
+text = '''
+…………………………._.;_'.-._
+……………………….{`--♥.-.'_,}
+… …………………{;..,__...-'/}
+....._.;_'.-._ …{..'-`.♥_;..-';
+...{`--.♥-.'_,}…`'--.._..-' ....._.;_'.-._
+.{;..,__...-'/}…._.;_'.-._ ...{`--.♥-.'_,}
+.{..'-`.._;..-';.{`--.♥-.'_,}.{;..,__.. .-'/}
+....`'--.._..-'.{;..,__...-'/.{..'-`.._ ;..-}
+........,--..,-"…`'--.._..- .....--.._..-
+...... ....._.;_'.-._..... ||......//
+..........{`--.♥-.'_,}.,-"-.....//.,-"-. .
+........{;..,__...-'/....'-..// (..'-...
+………..{..'-`.._;..-'. ,/. // ;---,
+......../....-'.)||......... /.,-"-....|
+........,---'`...||.......//. (..'-....|
+.....................|| ..//
+................../_\\Y//_|
+.................(_,-}={-,_)
+............... ...._//((_/
+.....................//))(\\
+....................(/..))
+........ ................(()
+.........................|)
+'''
+time.sleep(0.5)
+
+print(purple_color + text)
+" > present.py
 echo "cd ~ && python present.py" > ../usr/bin/m && chmod +x ../usr/bin/m
